bea7c218d88ca9df24fa65b21b7d29fe571ff319
ZachDaChampion <zachchampion79@gmail.com>
2019-12-24 22:52:04 -0500

    sidebar

--- point.js
+++ point.js
@@ -2,17 +2,17 @@ class Point {
 
   
   // constructor (who would've guessed)
-  constructor(x, y, angle, strength_next, strength_next) {
+  constructor(x, y, angle, strength_prev, strength_next) {
 
     this.x = x || 0;
     this.y = y || 0;
     this.angle = angle || 0;
-    this.strength_next = strength_next || 0;
+    this.strength_prev = strength_prev || 0;
     this.strength_next = strength_next || 0;
   }
 
 
   // generate control points from angle and strengths
-  generate_control_prev = () => new Point(this.x + this.strength_next * Math.cos(this.angle), this.y + this.strength_next * Math.sin(this.angle));
-  generate_control_next = () => new Point(this.x + this.strength_next * Math.cos(this.angle), this.y + this.strength_next * Math.sin(this.angle));
+  generate_control_prev = () => new Point(this.x + this.strength_next * Math.cos(this.angle), this.y + this.strength_prev * Math.sin(this.angle));
+  generate_control_next = () => new Point(this.x + this.strength_next * Math.cos(this.angle), this.y + this.strength_prev * Math.sin(this.angle));
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,35 @@
+let canvas = null;
+let canvas_holder = null;
+let card_holder = null;
+
 function setup() {
 
-	createCanvas(windowWidth, windowHeight);
+	canvas = createCanvas(windowHeight, windowHeight);
+	
+	canvas_holder = document.getElementById('canvas_holder');
+	canvas_holder.style.width = windowHeight.toString() + "px";
+	canvas_holder.style.height = windowHeight.toString() + "px";
+
+	card_holder = document.getElementById('cards_holder');
+	card_holder.style.width = (windowWidth - windowHeight - 4).toString() + "px";
+	card_holder.style.height = windowHeight.toString() + "px";
+
+	canvas.parent(canvas_holder);
+
+	windowResized();
+
+	['test 1', 'test 2', 'test 3', 'test 1', 'test 2', 'test 3', 'test 1', 'test 2', 'test 3'].forEach(title => {
+		let card = document.getElementById('card_template').content.cloneNode(true);
+		card.querySelector('#title').innerText = title;
+		card_holder.appendChild(card);
+	});
 }
 
 function draw() {
 	
 	background(0);
+}
+
+function windowResized() {
+	canvas.resize(windowHeight, windowHeight);
 }
